2066a01d0ba1eaa09610fe730d289dd28cc87c5f
s3662507 <s3662507@student.rmit.edu.au>
2020-03-06 15:15:43 +1100

    Setup of Chatting System

---
+++ src/main/java/com/l8z/L8zApplication.java
@@ -0,0 +1,13 @@
+package com.l8z;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L8zApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L8zApplication.class, args);
+	}
+
+}
