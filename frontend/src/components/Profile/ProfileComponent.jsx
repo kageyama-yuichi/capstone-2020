8410bdaabbd4738369358401fa971b5557a8bda5
Yuichi <S3718342@student.rmit.edu.au>
2020-04-16 17:04:20 +1000

    Added node-sass to dependencies and restyled profile

--- frontend/src/components/Profile/ProfileComponent.jsx
+++ frontend/src/components/Profile/ProfileComponent.jsx
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./ProfileComponent.css";
 import ProfileResources from "./ProfileResources.js";
+import {Form, Col, Button, Image} from "react-bootstrap";
 
 //TODO: Prevent XSS
 
@@ -17,49 +18,50 @@ class ProfileComponent extends Component {
 			imageError: "",
 			firstnameError: "",
 			lastnameError: "",
-			addressError: ""
+			addressError: "",
+			validated: false,
 		};
 		this.handleImageChange = this.handleImageChange.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleCancelClick = this.handleCancelClick.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.handleCancelClick = this.handleCancelClick.bind(this);
 	}
 
-    handleCancelClick() {
-        this.props.history.goBack();
-    }
+	handleCancelClick() {
+		this.props.history.goBack();
+	}
 
-	handleImageClick = e => {
+	handleImageClick = (e) => {
 		this.imageInputElement.click();
 	};
 
-	handleImageChange = e => {
+	handleImageChange = (e) => {
 		const imageFile = e.target.files[0];
 		console.log(imageFile);
 		let imageValid = true;
 
 		if (!imageFile) {
 			imageValid = false;
-			this.setState({ imageError: "Please select a valid file" });
+			this.setState({imageError: "Please select a valid file"});
 		} else if (!imageFile.name.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG)$/)) {
 			imageValid = false;
-			this.setState({ imageError: "Please select a valid file" });
+			this.setState({imageError: "Please select a valid file"});
 		} else if (imageFile.size > 500000) {
 			//Image size must be less than 500KB
 			imageValid = false;
-			this.setState({ imageError: "Image size must be less than 500KB" });
+			this.setState({imageError: "Image size must be less than 500KB"});
 		}
 
 		if (imageValid) {
 			this.setState({
-				picUrl: URL.createObjectURL(e.target.files[0])
+				picUrl: URL.createObjectURL(e.target.files[0]),
 			});
 		}
 	};
 
-	handleChange = e => {
-		const { name: fieldName, value } = e.target;
+	handleChange = (e) => {
+		const {name: fieldName, value} = e.target;
 		this.setState({
-			[fieldName]: value
+			[fieldName]: value,
 		});
 	};
 
@@ -72,35 +74,36 @@ class ProfileComponent extends Component {
 		//First name cannot be empty or contain numbers
 		if (!fields.firstname) {
 			formIsValid = false;
-			this.setState({ firstnameError: "First name cannot be empty" });
+			this.setState({firstnameError: "First name cannot be empty"});
 		} else if (!nameRegex.test(fields.firstname)) {
 			formIsValid = false;
 			this.setState({
-				firstnameError: "First name can only contain letters"
+				firstnameError: "First name can only contain letters",
 			});
 		}
 
 		//Last name cannot be empty or contain numbers
 		if (!fields.lastname) {
 			formIsValid = false;
-			this.setState({ lastnameError: "Last name Cannot be empty" });
+			this.setState({lastnameError: "Last name Cannot be empty"});
 		} else if (!nameRegex.test(fields.lastname)) {
 			formIsValid = false;
 			this.setState({
-				lastnameError: "Last name can only contain letters"
+				lastnameError: "Last name can only contain letters",
 			});
 		}
 
 		if (!fields.address) {
 			formIsValid = false;
-			this.setState({ addressError: "Address Cannot be empty" });
-        }
-        
-        return formIsValid;
+			this.setState({addressError: "Address Cannot be empty"});
+		}
+
+		return formIsValid;
 	}
 
-	onSubmit = e => {
-		e.preventDefault();
+	onSubmit = (e) => {
+		const form = e.currentTarget;
+
 		if (this.handleValidation()) {
 			let prof = {
 				username: this.state.username,
@@ -108,136 +111,146 @@ class ProfileComponent extends Component {
 				lname: this.state.lastname,
 				address: this.state.address,
 				bio: this.state.bio,
-				imagePath: this.state.picUrl
-			}
+				imagePath: this.state.picUrl,
+			};
 			// Retrieve the User's Profile Information from the Server
-			ProfileResources.updateUserProfile(this.state.username, prof)
+			ProfileResources.updateUserProfile(this.state.username, prof);
 			console.log("success");
 			this.props.history.push(`/dashboard`);
+		} else {
+			e.preventDefault();
 		}
+
+		this.setState({validated: true});
 	};
 
 	refreshUserProfile = () => {
 		// Retrieve the User's Profile Information from the Server
-		ProfileResources.receiveUserProfile(this.state.username)
-		.then(response => 
-		{
+		ProfileResources.receiveUserProfile(this.state.username).then((response) => {
 			console.log(response.data);
 			this.setState({
 				firstname: response.data.fname,
 				lastname: response.data.lname,
 				address: response.data.address,
 				bio: response.data.bio,
-				picUrl: response.data.imagePath
-			})
+				picUrl: response.data.imagePath,
+			});
 		});
-	}
-	
+	};
+
 	componentDidMount() {
 		this.refreshUserProfile();
 	}
-	
+
 	render() {
 		return (
 			<div className="app-window profile-component">
 				<header className="title-container">Profile</header>
-				<form
+				<Form
+					noValidate
+					validated={this.state.validated}
 					className="profile-update-form"
-					onSubmit={this.onSubmit.bind(this)}
-				>
+					onSubmit={this.onSubmit.bind(this)}>
 					<div className="image-upload-wrapper">
-						<div className="image-upload">
-							<img
+						<Form.Group className="image-upload">
+							<Image
+								width="200"
+								height="200"
 								className="image-upload-current"
 								src={this.state.picUrl}
+								roundedCircle
 							/>
-							<div
-								className="image-upload-overlay"
-								onClick={this.handleImageClick}
-							>
+							<div className="image-upload-overlay" onClick={this.handleImageClick}>
 								<div className="overlay-text">Upload Image</div>
 							</div>
 							<input
 								className="image-upload-input"
 								type="file"
-								ref={input => (this.imageInputElement = input)}
+								ref={(input) => (this.imageInputElement = input)}
 								onChange={this.handleImageChange}
 							/>
-						</div>
-						<div
-							className="help-text"
-							onClick={this.handleImageClick}
-						>
+						</Form.Group>
+						<div className="help-text" onClick={this.handleImageClick}>
 							Click to change profile picture
 						</div>
-						<div className="image-upload-error">
-							{this.state.imageError}
-						</div>
-						<div className="cancel-button-container">
-							<button className="cancel-button submit-button"onClick={this.handleCancelClick}>
-								CANCEL
-							</button>
-						</div>
+						<div className="image-upload-error">{this.state.imageError}</div>
+						<div className="cancel-button-container"></div>
 					</div>
 					<div className="info-container">
 						<div className="container-title">
 							<h2>Change your Profile details </h2>
 						</div>
-						<div className="name-container">
-							<div className="fName-container">
-								<h3>First name</h3>
-								<input
-									className="fName-input input-field"
+						<Form.Row>
+							<Form.Group as={Col} controlId="validationCustom01">
+								<Form.Label>First name</Form.Label>
+								<Form.Control
+									required
+									className="fName-input"
 									name="firstname"
 									type="text"
 									value={this.state.firstname}
 									onChange={this.handleChange.bind(this)}
-                                ></input>
-                                <p className="form-error">{this.state.firstnameError}</p>
-							</div>
-							<div className="lName-container">
-								<h3>Last name</h3>
-								<input
+								/>
+								<Form.Control.Feedback type="invalid">
+									{this.state.firstnameError}
+								</Form.Control.Feedback>
+							</Form.Group>
+
+							<Form.Group as={Col}>
+								<Form.Label>Last name</Form.Label>
+								<Form.Control
+									required
 									className="lName-input input-field"
 									name="lastname"
 									type="text"
 									value={this.state.lastname}
 									onChange={this.handleChange.bind(this)}
-                                ></input>
-                                <p className="form-error">{this.state.lastnameError}</p>
-							</div>
-						</div>
-						<div className="address-container">
-							<h3>Address</h3>
-							<input
+								/>
+								<Form.Control.Feedback type="invalid">
+									{this.state.lastnameError}
+								</Form.Control.Feedback>
+							</Form.Group>
+						</Form.Row>
+
+						<Form.Group className="address-container">
+							<Form.Label>Address</Form.Label>
+							<Form.Control
+								required
 								className="address-input input-field"
 								name="address"
 								type="text"
 								value={this.state.address}
 								onChange={this.handleChange.bind(this)}
-                            ></input>
-                            <p className="form-error">{this.state.addressError}</p>
-						</div>
-						<div className="bio-container">
-							<h3>Biography</h3>
-							<textarea
-								className="bio-input"
+							/>
+							<Form.Control.Feedback type="invalid">
+								{this.state.addressError}
+							</Form.Control.Feedback>
+						</Form.Group>
+
+						<Form.Group className="bio-container">
+							<Form.Label>Biography</Form.Label>
+							<Form.Control
+								rows="15"
+								as="textarea"
 								name="bio"
 								form="profile-update-form"
 								value={this.state.bio}
 								onChange={this.handleChange.bind(this)}
 								placeholder="Create a bio!"
-							></textarea>
-						</div>
-						<div className="submit-container">
-							<input
-								type="submit"
-								className="profile-update-button submit-button"
-								value="SAVE CHANGES"
-							></input>
-						</div>
+							/>
+						</Form.Group>
+						<Form.Row className="justify-content-end">
+							<Form.Group md="1" as={Col}>
+								<Button type="button" variant="outline-primary" onClick={this.handleCancelClick}>
+									CANCEL
+								</Button>
+							</Form.Group>
+							<Form.Group md="4" as={Col}>
+								<Button type="submit" variant="secondary">SAVE CHANGES</Button>
+							</Form.Group>
+						</Form.Row>
 					</div>
-				</form>
+				</Form>
 			</div>
 		);
 	}
