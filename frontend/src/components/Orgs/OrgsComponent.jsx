0de9ea6dc858d09f1f73eec5d19e04d9624189bd
Yuichi <S3718342@student.rmit.edu.au>
2020-04-18 15:43:28 +1000

    Standardized form validation for AddOrgsComponent

--- frontend/src/components/Orgs/OrgsComponent.jsx
+++ frontend/src/components/Orgs/OrgsComponent.jsx
@@ -1,7 +1,9 @@
-import React, {Component} from 'react'
-import OrgsResources from './OrgsResources.js'
-import { API_URL } from '../../Constants'
-import './OrgsComponent.css'
+import React, {Component} from "react";
+import OrgsResources from "./OrgsResources.js";
+import {API_URL} from "../../Constants";
+import "./OrgsComponent.css";
+import { Link} from "react-router-dom";
+
 
 /*
 	Left to do:
@@ -10,49 +12,45 @@ import './OrgsComponent.css'
 */
 
 class OrgsComponent extends Component {
-	constructor (props) {
+	constructor(props) {
 		super(props);
 		this.state = {
-			username: this.props.match.params.username,
-			orgs: []
+			username: localStorage.getItem("username"),
+			orgs: [],
 		};
-		this.handle_create_org = this.handle_create_org.bind(this);
 		this.handle_delete_org = this.handle_delete_org.bind(this);
 	}
 
-	handle_create_org = () => {
-		var url = this.state.username+'/new';
-		this.props.history.push(url);
-	}
 	handle_delete_org = (org_id) => {
 		OrgsResources.delete_org(this.state.username, org_id);
 		// Reset using this.refresh_orgs in Callback to Force
-		this.setState({
-			orgs: []
-		}, () => {
-			this.refresh_orgs();
-		})
-	}
+		this.setState(
+			{
+				orgs: [],
+			},
+			() => {
+				this.refresh_orgs();
+			}
+		);
+	};
 	handle_update_org = (org_id) => {
-		var url = this.state.username+"/"+org_id;
+		var url = this.state.username + "/" + org_id;
 		this.props.history.push(url);
-	}
-	
+	};
+
 	componentDidUpdate() {
 		console.log(this.state.orgs);
 	}
-	
+
 	refresh_orgs = () => {
 		// Retrieves the Organisations of the User from the Server
-		OrgsResources.retrieve_orgs(this.state.username)
-		.then(response => 
-		{
+		OrgsResources.retrieve_orgs(this.state.username).then((response) => {
 			// Maps the Response Data (Orgs.class) to JSObject
-			for(let i=0; i<response.data.length; i++){
-				var user_role_temp = '';
+			for (let i = 0; i < response.data.length; i++) {
+				var user_role_temp = "";
 				// Find the User's Role
-				for(let j=0; j<response.data[i].members.length; j++){
-					if(response.data[i].members[j].username === this.state.username){
+				for (let j = 0; j < response.data[i].members.length; j++) {
+					if (response.data[i].members[j].username === this.state.username) {
 						user_role_temp = response.data[i].members[j].role;
 						break;
 					}
@@ -61,33 +59,34 @@ class OrgsComponent extends Component {
 					org_id: response.data[i].org_id,
 					org_title: response.data[i].org_title,
 					user_role: user_role_temp,
-					members: response.data[i].members
-				})
+					members: response.data[i].members,
+				});
 				this.setState({
-					orgs: this.state.orgs
-				})
+					orgs: this.state.orgs,
+				});
 			}
 		});
-	}
-	
+	};
+
 	componentDidMount() {
 		this.refresh_orgs();
 	}
-	
+
 	render() {
-		console.log("System - Rendering Page...");
-        return (
-            <div className="OrgComponent">
-				<h1>L8Z Organisations</h1>
-				<input
-					className="new_organisation"
-					type="button"
-					value="+"
-					onClick={this.handle_create_org}
-				/>
-				<h2>Your Organisations</h2>
-				{this.state.orgs.map(org =>
-					<div key={org.org_id} className='orgs'>
+		return (
+			<div className="app-window org-component">
+				<header className="title-container">
+					<div className="title-flex">
+						<div className="title-div">Organisations</div>
+						<Link to="/orgs/new" className="new-org-button">
+							
+							Create Org
+						</Link>
+					</div>
+				</header>
+
+				{this.state.orgs.map((org) => (
+					<div key={org.org_id} className="orgs">
 						<input
 							className="delete_organisation"
 							type="button"
@@ -102,15 +101,15 @@ class OrgsComponent extends Component {
 						/>
 						<h3 key={org.org_id}>{org.org_title}</h3>
 						<div>
-							{org.members.map(member =>
+							{org.members.map((member) => (
 								<p key={member.username}>{member.username}</p>
-							)}
+							))}
 						</div>
 					</div>
-				)}
+				))}
 			</div>
-        )
-    }
+		);
+	}
 }
 
-export default OrgsComponent
+export default OrgsComponent;
