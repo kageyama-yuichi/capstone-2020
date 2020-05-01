71eef9135387cffdc5e11c7221819d0f1dbd37b4
s3664411 <s3664411@student.rmit.edu.au>
2020-05-01 14:45:01 +1000

    Fix @GetMapping for Contact List

--- frontend/src/components/Dashboard/DashboardComponent.jsx
+++ frontend/src/components/Dashboard/DashboardComponent.jsx
@@ -2,24 +2,14 @@ import React, {Component} from "react";
 import "./DashboardComponent.css";
 import TodoComponent from "../Todo/TodoComponent";
 import {Container, Row, Col} from "react-bootstrap";
-import ContactsResource from "../Contacts/ContactsResource.js"
-
 
 class DashboardComponent extends Component {
-
-onClick() {
-	let username = sessionStorage.getItem("authenticatedUser");
-	ContactsResource.addContact(username, "Tyler (ScrumptiousMaster)")
-}
-
 	render() {
 		return (
 			<div className="app-window dashboard-component">
 				<Container fluid>
 					<Row>
-						<Col>
-						     <button onClick={this.onClick}>Touch this to make a friend</button>
-						</Col>
+						<Col></Col>
 						<Col>
 							<TodoComponent />
 						</Col>
