5b842d658291bbace44e8cd49536437081505ef5
s3488937 <s3488937@student.rmit.edu.au>
2020-05-02 22:05:26 +1000

    Removed useless code

--- backend/src/main/java/com/l8z/resources/UserJpaResource.java
+++ backend/src/main/java/com/l8z/resources/UserJpaResource.java
@@ -121,20 +121,4 @@ public class UserJpaResource {
     	}
     	return inviter_basic_users;
     }
-    @GetMapping("/jpa/profile/{username}/{password}") 
-	public User receiveUserPassword(@PathVariable String password){		
-		return repo.findByPassword(password);
-	}
-    @PostMapping("/jpa/profile/{username}/{password}")
-	public ResponseEntity<Void> updateUserPassword(
-			@PathVariable String username, 
-			@RequestBody User profile
-		){
-		User userUpdate = repo.getOne(profile.getID());
-		userUpdate.setPassword(profile.getPassword());
-		
-		// Update Profile
-		repo.save(userUpdate);
-		return ResponseEntity.noContent().build();
-	} 
 }
