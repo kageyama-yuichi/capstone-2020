068e10d9aa204bab17e81295bd06f4a38e8b4bb4
Hung Tran <s3488831@student.rmit.edu.au>
2020-05-15 05:28:24 +1000

    Fixed comments

--- frontend/src/test/ChatTest.test.js
+++ frontend/src/test/ChatTest.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ChatComponent from '../components/Chat/ChatComponent';
 
-    describe('Profile component tests', ()=> {
+    describe('Chat component tests', ()=> {
         const wrapper = shallow(<ChatComponent />);
 		
 		it('Should render Chat components', ()=> {
--- frontend/src/test/ContactsTest.test.js
+++ frontend/src/test/ContactsTest.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ContactComponent from '../components/Contacts/ContactComponent';
 
-    describe('Chat component tests', ()=> {
+    describe('Contact component tests', ()=> {
         const wrapper = shallow(<ContactComponent />);
 		
 		it('Should render contact components', ()=> {
